ab5cd0cf1e00fabb98316047cd073852472b0bfc
Scotty Eckenthal <scott.eckenthal@gmail.com>
2025-02-13 22:39:25 -0500

    redux instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, selectCount } from './store/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
 
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => dispatch(increment())}>
           count is {count}
         </button>
         <p>
