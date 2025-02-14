fb079e8f67ac7c9f931e240de8cbe97545ed200a
Scotty Eckenthal <scott.eckenthal@gmail.com>
2025-02-13 22:57:54 -0500

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, selectCount } from "./store/counterSlice";
 
@@ -6,7 +6,16 @@ function App() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
-  return <button onClick={() => dispatch(increment())}>count is {count}</button>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button
+        onClick={() => dispatch(increment())}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+      >
+        count is {count}
+      </button>
+    </div>
+  );
 }
 
 export default App;
