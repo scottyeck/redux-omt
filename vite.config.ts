5970eb8316659241d3a7ae7aff4a76201247a323
Scotty Eckenthal <scott.eckenthal@gmail.com>
2025-02-13 23:03:22 -0500

    install comlink

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import comlink from "vite-plugin-comlink";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), comlink()],
+  worker: {
+    plugins: [comlink()],
+  },
+});
